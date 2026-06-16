b0d1d1c6c918eb38fbfec9ef7839ece8c2374e28
Juan Marley <2024301738@aluno.canoas.ifrs.edu.br>
2026-06-16 09:41:08 -0300

    CRRdb

--- info.php
+++ info.php
@@ -1,4 +1,8 @@
 <?php
+var_dump(getenv('MYSQLHOST'));
+var_dump(getenv('MYSQLPORT'));
+var_dump(getenv('MYSQLDATABASE'));
+var_dump(getenv('MYSQLUSER'));
 echo "<pre>";
 print_r(PDO::getAvailableDrivers());
 echo "</pre>";
