ed0f6766f2205bd3b07a971ce42a12c0030f66d8
playerDragon893 <2024301738@aluno.canoas.ifrs.edu.br>
2026-06-19 23:37:10 -0300

    Fix include path for conexaodb.php

--- src/modules/plantas/cardPlanta.php
+++ src/modules/plantas/cardPlanta.php
@@ -1,5 +1,5 @@
 <?php 
-    include __DIR__ . '../../shared/conexaodb.php';
+    include __DIR__ . '/../../shared/conexaodb.php';
     $sql = "SELECT id, nome_comum, descricao FROM plantas";
 
     $stmt = $conexao->query($sql);
