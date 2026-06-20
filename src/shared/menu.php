cfed2b4d1015764a948f3c57c294ff900b701108
playerDragon893 <2024301738@aluno.canoas.ifrs.edu.br>
2026-06-20 06:47:41 -0300

    Remove session start check in menu.php

--- src/shared/menu.php
+++ src/shared/menu.php
@@ -1,7 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
 $logado = isset($_SESSION['id']);
 $isAdmin = !empty($_SESSION['admin']);
 $nomeUsuario = $_SESSION['nome'] ?? '';
