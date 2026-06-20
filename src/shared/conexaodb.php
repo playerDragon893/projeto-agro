436a0337cfe857d4a497d4f110987fc76a132a17
playerDragon893 <2024301738@aluno.canoas.ifrs.edu.br>
2026-06-19 23:31:04 -0300

    Update conexaodb.php

--- src/shared/conexaodb.php
+++ src/shared/conexaodb.php
@@ -5,24 +5,10 @@ $dbname   = getenv('MYSQLDATABASE');
 $username = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "plantadb";
-
 try {
     $conexao = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-    $conexao = new PDO(
-        "mysql:host=$servername;dbname=$dbname",
-        $username,
-        $password
-    );
-
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
     die("Erro na conexão: " . $e->getMessage());
 }
+?>
